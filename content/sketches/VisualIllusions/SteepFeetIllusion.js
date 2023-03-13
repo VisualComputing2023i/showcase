b8ea4cc1dde8ea1695aa2cb68393cb09e1dead07
wolfGhost23 <crvargasm@unal.edu.co>
2023-03-13 13:06:36 -0500

    stepping add selector

--- content/sketches/VisualIllusions/SteepFeetIllusion.js
+++ content/sketches/VisualIllusions/SteepFeetIllusion.js
@@ -1,40 +1,65 @@
-let widthRects = 30;
-let widthSteps = 60;
-let sliderSpeed;
-let checkboxConstrast;
-let speed = 0;
+let widthRects = 30
+let widthSteps = 60
+let sliderSpeed
+let checkboxConstrast
+let colorSelector
+let speed = 0
+let colorA
+let colorB
+
 
 function setup() {
-    createCanvas(675, 400);
+    createCanvas(675, 400)
 
-    sliderSpeed = createSlider(1 / 4, 16 / 4, 6 / 4, 1 / 4);
-    sliderSpeed.position(10, 10);
-    sliderSpeed.style('width', '80px');
+    sliderSpeed = createSlider(1 / 4, 16 / 4, 6 / 4, 1 / 4)
+    sliderSpeed.position(10, 10)
+    sliderSpeed.style('width', '80px')
 
-    checkboxConstrast = createCheckbox('Hi-cont', false);
-    checkboxConstrast.position(10, 35);
+    checkboxConstrast = createCheckbox('Hi-cont', false)
+    checkboxConstrast.position(10, 35)
 
-    let span = createSpan('Speed');
-    span.position(105, 12);
+    colorSelector = createSelect()
+    colorSelector.position(90, 37.5)
+    colorSelector.option("Amarillo-Azul")
+    colorSelector.option("Rojo-Verde")
+    colorSelector.option("Negro-Blanco")
+    colorSelector.selected("Negro-Blanco")
+
+    let span = createSpan('Speed')
+    span.position(105, 12)
 }
 
 function draw() {
-    background(255, 255, 255);
-    noStroke();
-
-    checkboxConstrast.checked() ? fill(color(207, 207, 207)) : fill(color(0, 0, 0));
+    background(255, 255, 255)
+    noStroke()
+    checkboxConstrast.checked() ? fill(color(207, 207, 207)) : fill(color(0, 0, 0))
 
     for (let i = 0; i < (width / widthRects); i++) {
-        rect(widthRects * 2 * i, 0, widthRects, height);
+        rect(widthRects * 2 * i, 0, widthRects, height)
     }
 
-    fill(color(0, 0, 0));
-    rect((speed % (width + widthSteps)) - widthSteps, 150, widthSteps, widthSteps / 2);
-    checkboxConstrast.checked() ? fill(color(175, 175, 175)) : fill(color(255, 255, 255))
-    rect((speed % (width + widthSteps)) - widthSteps, 225, widthSteps, widthSteps / 2);
+    switch (colorSelector.value()) {
+        case "Amarillo-Azul":
+            colorA = color(0, 0, 255)
+            colorB = color(255, 255, 0)
+            break
+        case "Rojo-Verde":
+            colorA = color(0, 128, 0)
+            colorB = color(128, 0, 0)
+            break
+        case "Negro-Blanco":
+            colorA = color(0, 0, 0)
+            checkboxConstrast.checked() ? colorB = color(175, 175, 175) : colorB = color(255, 255, 255)
+            break
+    }
 
-    speed += sliderSpeed.value();
+    fill(colorA)
+    rect((speed % (width + widthSteps)) - widthSteps, 120, widthSteps, widthSteps / 2)
+    fill(colorB)
+    rect((speed % (width + widthSteps)) - widthSteps, 255, widthSteps, widthSteps / 2)
 
-    fill('#99ff66');
-    rect(0, 0, 150, 60);
+    speed += sliderSpeed.value()
+
+    fill('#99ff66')
+    rect(0, 0, 195, 60)
 }
