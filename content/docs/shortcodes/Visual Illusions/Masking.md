49e180343fb34a5e279e307b89684aba6a3dc2ec
wolfGhost23 <crvargasm@unal.edu.co>
2023-03-27 21:06:33 -0500

    kinegram js effect ok

--- content/docs/shortcodes/Visual Illusions/Masking.md
+++ content/docs/shortcodes/Visual Illusions/Masking.md
@@ -1,5 +1,5 @@
 ---
-weight: 2
+weight: 3
 ---
 
 ## Visual masking
@@ -10,3 +10,33 @@ weight: 2
 Implement a kinegram and some moiré patterns which are close related visual phenomena to masking.
 
 {{< /hint >}}
+
+### Introducción
+
+Que vamos a encontrar???
+
+### Antecedentes
+
+Un poco de historia :3
+### Solución
+
+Faltan las instrucciones de uso
+
+{{< p5-iframe sketch="/showcase/sketches/VisualIllusions/masking/kinegram.js" width="700" height="624" >}}
+
+### Código 
+
+{{< details "Title" open >}}
+### Markdown content
+
+Lorem markdownum insigne...
+
+{{< /details >}}
+
+### Conclusiones
+
+Esto sirve para....
+
+
+
+[1] https://www.giannisarcone.com/Kinegrams.html#
---
+++ content/sketches/VisualIllusions/masking/kinegram.js
@@ -0,0 +1,33 @@
+let img
+let widthRects = 4.4
+let speed = 0
+let run = true
+
+function preload() {
+    var hostURL = "http://localhost:1313/showcase/";
+    img = loadImage(hostURL + 'sketches/VisualIllusions/masking/warping.jpg')
+}
+
+function mouseClicked() {
+    run = !run
+}
+
+function doubleClicked() {
+    speed = 0
+}
+
+function setup() {
+    createCanvas(680, 600)
+}
+
+function draw() {
+    background(255)
+    noStroke()
+    image(img, (width - 600) / 2, 0, 600, 600)
+
+    fill(color(0, 0, 0))
+    for (let i = 0; i < 80; i++) {
+        rect((widthRects * 2 * i) + speed - 150, 0, widthRects, height)
+    }
+    run ? speed = ((speed + 0.2) % (width / 3)) : ''
+} 
