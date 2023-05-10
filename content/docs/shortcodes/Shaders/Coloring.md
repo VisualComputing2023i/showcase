0d3641b334e01cf559d7d80eea2916f1e85f782f
wolfGhost23 <crvargasm@unal.edu.co>
2023-05-10 10:57:18 -0500

    add coloring shader & page

--- content/docs/shortcodes/Shaders/Coloring.md
+++ content/docs/shortcodes/Shaders/Coloring.md
@@ -8,4 +8,4 @@ weight: 2
 
 Blending two colors is implemented by doing a component-wise multiplication between them:
 
-{{< p5-iframe sketch="/showcase/sketches/shaders/coloring/blend.js" lib1="https://cdn.jsdelivr.net/gh/VisualComputing/p5.treegl/p5.treegl.js" width="700" height="540" >}}
+{{< p5-iframe sketch="/showcase/sketches/shaders/coloring/blend.js" lib1="https://cdn.jsdelivr.net/gh/VisualComputing/p5.treegl/p5.treegl.js" width="320" height="330" >}}
---
+++ content/sketches/shaders/coloring/blend.js
@@ -0,0 +1,68 @@
+let blendShader, brightness, color1, color2, radius
+
+function preload() {
+    blendShader = readShader('/showcase/sketches/shaders/coloring/blend.frag')
+}
+
+function setup() {
+    createCanvas(300, 300, WEBGL)
+    colorMode(RGB, 1) //255 -> 1
+    rectMode(RADIUS)  //centering the (0,0) (x,y) origin point 
+    radius = width / 5
+    noStroke()
+    color1 = createColorPicker(color(0.8, 0.5, 0.3))
+    color1.position(10, 10)
+    color2 = createColorPicker(color(0.9, 0.1, 0.4))
+    color2.position(width / 2 + 10, 10)
+
+    brightness = createSlider(0, 1, 0.5, 0.05) //brillo entre 0 y 1
+    brightness.position(width / 2 - 35, height / 2)
+    brightness.style('width', '80px')
+
+    shader(blendShader) //Llamamos al shader del .frag
+}
+
+function draw() {
+    // rectMode, e.g., rectMode(RADIUS) requires Tree.pmvMatrix
+    // so no square but beginShape / endShape
+
+    let l = 0.8
+    let offset = (1 - l) / 2
+    let color1Color = color1.color()
+    let color2Color = color2.color()
+
+    background(0)
+
+    blendShader.setUniform('brightness', 1.0)
+    blendShader.setUniform('uMaterial1', [red(color1Color), green(color1Color), blue(color1Color), 1.0])
+    blendShader.setUniform('uMaterial2', [1.0, 1.0, 1.0, 1.0])
+
+    beginShape()
+    vertex(-offset - l, +offset, 0)
+    vertex(-offset, +offset, 0)
+    vertex(-offset, +offset + l, 0)
+    vertex(-offset - l, +offset + l, 0)
+    endShape()
+
+    blendShader.setUniform('uMaterial1', [1.0, 1.0, 1.0, 1.0])
+    blendShader.setUniform('uMaterial2', [red(color2Color), green(color2Color), blue(color2Color), 1.0])
+    blendShader.setUniform('brightness', 1.0)
+
+    beginShape()
+    vertex(+offset, +offset, 0)
+    vertex(+offset + l, +offset, 0)
+    vertex(+offset + l, +offset + l, 0)
+    vertex(+offset, +offset + l, 0)
+    endShape()
+
+    blendShader.setUniform('uMaterial1', [red(color1Color), green(color1Color), blue(color1Color), 1.0])
+    blendShader.setUniform('uMaterial2', [red(color2Color), green(color2Color), blue(color2Color), 1.0])
+    blendShader.setUniform('brightness', brightness.value())
+
+    beginShape()
+    vertex(-l / 2, -offset - l, 0)
+    vertex(+l / 2, -offset - l, 0)
+    vertex(+l / 2, -offset, 0)
+    vertex(-l / 2, -offset, 0)
+    endShape()
+}
